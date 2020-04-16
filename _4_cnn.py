5fb52f76780e93d83e4a0a40ffdcaa09e788545e
Ceason Cui <ceasoncui@gmail.com>
2020-04-16 20:17:16 +0900

    epoch=50

--- _4_cnn.py
+++ _4_cnn.py
@@ -139,7 +139,7 @@ with tf.Session() as sess:
     sess.run(init)
     coord = tf.train.Coordinator() 
     threads=tf.train.start_queue_runners(sess=sess,coord=coord) 
-    for i in range(20):
+    for i in range(50):
         for j in range(n_batch):
             val, l = sess.run([img_batch, label_batch])
             l = one_hot(l,2)
