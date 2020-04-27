ad1c004f4e8571d64b7575d5784177714c9c7c0d
Ceason Cui <ceasoncui@gmail.com>
2020-04-27 19:39:46 +0900

    debug ROD

--- ReadOwnData.py
+++ ReadOwnData.py
@@ -13,7 +13,7 @@ def read_and_decode(filename): # 读入tfrecords
                                        })#将image数据和label取出来
  
     img = tf.decode_raw(features['img_raw'], tf.uint8)
-    img = tf.reshape(img, [1, 64, 64])  #reshape为64*64的1通道图片
+    img = tf.reshape(img, [64, 64])  #reshape为64*64的1通道图片
     #img = tf.cast(img, tf.float32)#在流中抛出img张量
     label = tf.cast(features['label'], tf.int32) #在流中抛出label张量
     #labels = tf.one_hot(label,2)
